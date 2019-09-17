53e3799731bc86c1c4a596fbe8016728253caf87
Rushi Patel <prushir@udel.edu>
2019-09-16 23:56:06 -0400

    good code

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 mp3: mp3.o
 	gcc mp3.o -o mp3
-mp3.o: mp3.c
+mp3.o: mp3.c mp3.h
 	gcc -c mp3.c -o mp3.o
 clean: 
 	rm *.o mp3
---
+++ mp3.h
@@ -0,0 +1,10 @@
+#ifndef mp3_H_
+#define mp3_H_
+
+void enter();
+void delete();
+void deleteMP3();
+void printListForward();
+void printListReverse();
+void freeList();
+#endif
