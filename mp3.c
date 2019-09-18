b0e557217b6f2ed370fbe3d46dd8f7aa7d6086b1
Rushi Patel <prushir@udel.edu>
2019-09-17 23:27:41 -0400

    Comments added

--- mp3.c
+++ mp3.c
@@ -1,3 +1,11 @@
+/**
+ * In this file you find the implemented methods from the mp3.h file.
+ * First we declare a struct of type mp3 and alias it as mp3_t, this struct has
+ * two char attributes, two int attributes, and two mp3 attributes.
+ * Below the mp3 declaration we have all of the method implementations followed by the
+ * main where the program is brought together in a menu.
+ **/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,22 +14,29 @@
 #define BUFFERSIZE 128
 
 typedef struct mp3 {
-    char *artist;
-    char *songName;
-    int year;
-    int runtime;
-    struct mp3 *next;
-    struct mp3 *prev;
+  char *artist;
+  char *songName;
+  int year;
+  int runtime;
+  struct mp3 *next;
+  struct mp3 *prev;
 } mp3_t;
 
+/**
+ * The enterData method takes in two parameters of type mp3 which holds the address 
+ * of a pointer. This method then asks the user for information to create an MP3 node. 
+ * It then adds the new node that was created to the end of the doubly linked list.
+ * There are three side effects from calling this method because it creates space on the heap
+ * for the new mp3 node, and the two character arrays for artist and song title.
+ **/
+
 void enterData(mp3_t **first, mp3_t **tail ){
-    char  buffer[BUFFERSIZE];
-    int   len;
-    mp3_t *mp3 = NULL;
-    printf("Enter an Artist: ");
-    getchar();
-  if (fgets(buffer, BUFFERSIZE , stdin) != NULL)
-  {
+  char buffer[BUFFERSIZE];
+  int len;
+  mp3_t *mp3 = NULL;
+  printf("Enter an Artist: ");
+  getchar();
+  if (fgets(buffer, BUFFERSIZE , stdin) != NULL){
     len = (int) strlen(buffer);
     buffer[len - 1] = '\0';                 
     mp3 = (mp3_t *) malloc(sizeof(mp3_t));
@@ -30,34 +45,39 @@ void enterData(mp3_t **first, mp3_t **tail ){
     strcpy(mp3->artist, buffer);
   }
 
-
   printf("Enter a Song Title: ");
-  if (fgets(buffer, BUFFERSIZE , stdin) != NULL)
-  {
+  if (fgets(buffer, BUFFERSIZE , stdin) != NULL){
     len = (int) strlen(buffer);
     buffer[len - 1] = '\0';                 
     mp3->songName = (char *) malloc(len);
     strcpy(mp3->songName, buffer);
   }
-    printf("Enter the Year of the Song: ");
-    scanf("%d",&mp3->year);
-    printf("Enter the Duration of the Song: ");
-    scanf("%d",&mp3->runtime);
-    getchar();
 
-    if(*first == NULL){
-        *first = mp3;
-        *tail = mp3;
-        (*first)->prev = NULL;
-    }
-    else{
-        mp3->prev = *tail;
-        (*tail)->next = mp3;
-        *tail = mp3;
-    }
+  printf("Enter the Year of the Song: ");
+  scanf("%d",&mp3->year);
+  printf("Enter the Duration of the Song: ");
+  scanf("%d",&mp3->runtime);
+  getchar();
 
-
+  if(*first == NULL){
+    *first = mp3;
+    *tail = mp3;
+    (*first)->prev = NULL;
+  }
+  else{
+    mp3->prev = *tail;
+    (*tail)->next = mp3;
+    *tail = mp3;
+  }
 }
+
+/**
+ * The removeNode method takes in two parameters of type mp3 which holds the address 
+ * of a pointer. This method will then ask the user for the artist they would like to remove from the list.
+ * It then traverses the entire list and checks for the artist name and if they match it will call the 
+ * removeMP3 method. Then it will free the space that was allocated for the artist to be deleted 
+ * character array. There are no side effects.
+ **/
 
 void removeNode(mp3_t **first, mp3_t **tail){
   char  buffer[BUFFERSIZE];
@@ -78,20 +98,23 @@ void removeNode(mp3_t **first, mp3_t **tail){
 
   while(tmp != NULL){
     if(strcmp(tmp->artist,artistDel)==0){
-        tmpDel = tmp;
-        tmp = tmp->next;
-        removeMP3(tmpDel,first,tail);
+      tmpDel = tmp;
+      tmp = tmp->next;
+      removeMP3(tmpDel,first,tail);
     }
     else{
       tmp = tmp->next;
     }
-
   }
   free(artistDel);
-
-
-
 }
+
+/**
+ * The removeMP3 method takes in three parameters of type mp3 which holds the address 
+ * of a pointer. This method will then check the location of the node to be removed and removes it from
+ * the list. Then it will free the space that node allocated for the artist, song name, and 
+ * the node itself. There are not side effects.
+ **/
 
 void removeMP3(mp3_t *deleteArtist, mp3_t **tmpFirst, mp3_t **tmpTail){
   mp3_t *tmpFree = NULL;
@@ -114,10 +137,14 @@ void removeMP3(mp3_t *deleteArtist, mp3_t **tmpFirst, mp3_t **tmpTail){
   free(tmpFree->artist);
   free(tmpFree->songName);
   free(tmpFree);
-
-
 }
 
+/**
+ * The forwardPrinting method takes in one parameter of type mp3 which holds address of 
+ * the first node. This method will then check to see if list is empty or not and will
+ * print the list forwards. There are no side effects.
+ * 
+ **/
 
 void forwardPrinting(mp3_t *first){
   if (first == NULL) { 
@@ -128,12 +155,18 @@ void forwardPrinting(mp3_t *first){
     while(first != NULL){
       printf("The Artist is %s, ", first->artist);
       printf("The Song title is %s, ", first->songName);
-      printf("The Year is: %d, ", first->year);
+      printf("The Year is %d, ", first->year);
       printf("and the Duration is %d minutes. \n", first->runtime);
       first=first->next;
     }
   }
 }
+
+/**
+ * The reversePrinting method takes in one parameter of type mp3 which holds address of 
+ * the tail node. This method will then check to see if list is empty or not and will
+ * print the list in reverse. There are no side effects.
+ **/
 
 void reversePrinting(mp3_t *tail){
   if(tail == NULL){ 
@@ -144,14 +177,19 @@ void reversePrinting(mp3_t *tail){
     while(tail != NULL){
       printf("The Artist is %s, ", tail->artist);
       printf("The Song title is %s, ", tail->songName);
-      printf("The Year is: %d, ", tail->year);
+      printf("The Year is %d, ", tail->year);
       printf("and the Duration is %d minutes. \n", tail->runtime);
       tail = tail->prev;
     }
-}
+  }
 }
 
-
+/**
+ * The clearHeap method takes in one parameter of type mp3 which holds address of 
+ * the first node. This method will then traverse the list and free the space that 
+ * was allocated for each node and its artist and song title character arrays. 
+ * There are no side effects.
+ **/
 
 void clearHeap(mp3_t *first){
   mp3_t *tmp = NULL;
@@ -163,9 +201,14 @@ void clearHeap(mp3_t *first){
     free(tmp->songName);
     free(tmp);
   }
-  
-
 }
+
+/**
+ * This is the main where the first and tail mp3 nodes are created and set to NULL. 
+ * Then it creates an option screen with 5 choices allowing the user to continuously enter data, 
+ * remove data, print forward and print in reverse. Then after the option screen is exited then it 
+ * calls the clearHeap method to free the allocated space.
+ **/
 
 int main(){
     mp3_t *first = NULL;
@@ -204,7 +247,7 @@ int main(){
           printf("Not a choice");
           break;
       } 
-    } while(choices != 'E');
+    } while(choices != '5');
       clearHeap(first);
 	return 0;
 }
