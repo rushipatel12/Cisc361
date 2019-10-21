914760928d878cf5cb3c85cf200d79a6d96f09bf
Rushi Patel <prushir@udel.edu>
2019-10-21 18:28:08 -0400

    sigtermmm

--- rushiShell/main.c
+++ rushiShell/main.c
@@ -9,16 +9,16 @@ int main( int argc, char **argv, char **envp )
   /* put signal set up stuff here */
   signal(SIGINT, sig_handler);
   signal(SIGTSTP, sig_handler);
-  signal(SIGTSTP, sig_handler);
+  signal(SIGTERM, sig_handler);
   return sh(argc, argv, envp);
 }
 
 void sig_handler(int sig_num)
 {
   /* define your signal handler */
-  sigignore(SIGINT);
-  sigignore(SIGTERM);
-  sigignore(SIGTSTP);
+  signal(SIGINT, sig_handler);
+  signal(SIGTSTP, sig_handler);
+  signal(SIGTERM, sig_handler);
   printf("\nIgnored Signal\n");
 }
 
