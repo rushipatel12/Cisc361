ef5638bc836205f678237d027585510e62c71fba
Rushi Patel <prushir@udel.edu>
2019-10-21 16:43:17 -0400

    kill

--- rushiShell/main.c
+++ rushiShell/main.c
@@ -9,9 +9,6 @@ int main( int argc, char **argv, char **envp )
   /* put signal set up stuff here */
   signal(SIGINT, sig_handler);
   signal(SIGTSTP, sig_handler);
-  signal(SIGQUIT, sig_handler);
-  signal(SIGTERM, sig_handler);
-
   return sh(argc, argv, envp);
 }
 
@@ -20,8 +17,6 @@ void sig_handler(int sig_num)
   /* define your signal handler */
   signal(SIGINT, sig_handler);
   signal(SIGTSTP, sig_handler);
-  signal(SIGQUIT, sig_handler);
-  signal(SIGTERM, sig_handler);
   printf("\nIgnored Signal\n");
 }
 
